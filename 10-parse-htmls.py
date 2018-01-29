df0abb55fe28aa8ac66923f709e3ca7793872884
nt <gim.kobayashi@gmail.com>
2018-01-29 11:32:54 +0900

    HTMLパーサ追加

--- 10-parse-htmls.py
+++ 10-parse-htmls.py
@@ -5,6 +5,61 @@ import glob
 import bs4
 
 import re
+
+import json
+
+import concurrent.futures
+terms = set()
 for name in glob.glob("bing-ranking-scrape/htmls/*"):
   term = re.search(r'htmls/(.*?)_', name).group(1)
-  print(term)
+  #print(term)
+  terms.add( term )
+
+def _map(arg):
+  key, terms = arg
+
+  objs = []
+  for term in terms:
+    for type in ["top", "tail"]:
+      try:
+        html = ( open('bing-ranking-scrape/htmls/{term}_{type}'.format(term=term, type=type)).read() )
+      except Exception as ex:
+        print(ex)
+        continue 
+      soup = bs4.BeautifulSoup( html, "lxml" )
+      [s.extract() for s in soup('script')]
+     
+      obj = {}
+      title = soup.title.text if soup.title else None
+      print( title )
+      obj["term"] = term
+      obj["type"] = type
+      obj["title"] = title
+      desc,keywords = None,None
+      for meta in soup.find_all("meta"):
+        if meta.get("name") == "description":
+          desc = meta.get("content")
+        if meta.get("name") == "keywords":
+          keywords = meta.get("content")
+      obj["desc"] = desc
+      obj["keywords"] = keywords
+
+      obj["body"] = soup.find("body").text if soup.find("body") else None
+      #print(body)
+      objs.append(obj)
+  return objs
+
+args = {}
+for index, term in enumerate(terms):
+  key = index%12
+  if args.get(key) is None:
+    args[key] = []
+  args[key].append(term)
+args = [ (key,terms) for key,terms in args.items() ]
+
+objs = []
+with concurrent.futures.ProcessPoolExecutor(max_workers=12) as exe:
+  for _objs in exe.map(_map, args):
+    for obj in _objs:
+      objs.append(obj)
+open("entities.json", "w").write( json.dumps(objs, indent=2, ensure_ascii=False) )
