211a4a4b634321ceadcd314f79800911296a0683
nardtree <gim.kobayashi@gmail.com>
2018-02-02 07:48:04 +0900

    トリプルやペアのデータセットを作成する

--- bing-ranking-scrape/scan_pair.py
+++ bing-ranking-scrape/scan_pair.py
@@ -13,19 +13,32 @@ for name in glob.glob('terms/*'):
   obj = json.loads( open(name).read() )
 
   obj = list(filter(lambda x: re.search(r'^.*?/$', x[3]) is not None and x[3].count('/') == 3, obj) )
-  try:
+
+  # print(len(obj))
+  if len(obj) < 2:
+    continue
+
+  if len(obj) == 2:
     term = obj[0][0]
-    top = obj[0][3]
+    r1url = obj[0][3]
     r1 = obj[0][1] 
 
-    tail = obj[-1][3]
+    r2url = obj[-1][3]
     r2 = obj[-1][1]
-    if top == tail:
-      continue
-    print(term, top, tail, r1, r2)
-    term_data[term] = (top, tail, r1, r2)
-  except Exception as ex:
-    #os.remove(name)
-    ...
+    print(term, r1url, r2url, r1, r2)
+    term_data[term] = (r1url, r2url, r1, r2)
+  if len(obj) > 2:
+    term = obj[0][0]
+    r1url = obj[0][3]
+    r1 = obj[0][1] 
+    
+    r2url = obj[1][3]
+    r2 = obj[1][1] 
+
+    r3url = obj[-1][3]
+    r3 = obj[-1][1]
+
+    print(term, r1url, r2url, r3url, r1, r2, r3)
+    term_data[term] = (r1url, r2url, r3url, r1, r2, r3)
 
 open('term_data.pkl', 'wb').write( pickle.dumps(term_data) )
