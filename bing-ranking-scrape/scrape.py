05f018b608b5deed65bc3992a92c034bebfeeff7
nardtree <gim.kobayashi@gmail.com>
2018-02-02 07:33:29 +0900

    名詞リストを追加

--- bing-ranking-scrape/scrape.py
+++ bing-ranking-scrape/scrape.py
@@ -15,7 +15,7 @@ import os
 import concurrent.futures
 encode = urllib.parse.quote
 
-term_freq = json.loads(open('./term_freq.json').read())
+nouns = json.load(open('nouns.json'))
 
 url = 'https://www.bing.com/search?q={}'
 
@@ -24,28 +24,32 @@ def _map(arr):
   for term in terms:
     if os.path.exists('terms/{}.json'.format(term)) is True:  
       continue
-    print(term, encode(term), freq)
-    r = requests.get( url.format(encode(term)) )
-    html = r.text
-    soup = bs4.BeautifulSoup(html)
+    try:
+      print(term, encode(term))
+      r = requests.get( url.format(encode(term)) )
+      html = r.text
+      soup = bs4.BeautifulSoup(html)
 
-    data = []
-    for index, h2 in enumerate(soup.find_all('h2')):
-      page_name = h2.text
-      if h2.find('a') is None:
-        continue
-      _url = h2.find('a').get('href')
+      data = []
+      for index, h2 in enumerate(soup.find_all('h2')):
+        page_name = h2.text
+        if h2.find('a') is None:
+          continue
+        _url = h2.find('a').get('href')
 
-      print(term, index, page_name, _url)
-      data.append( (term, index, page_name, _url) )
-    open('terms/{}.json'.format(term), 'w').write( json.dumps(data, indent=2, ensure_ascii=False) )
+        print(term, index, page_name, _url)
+        data.append( (term, index, page_name, _url) )
+      open('terms/{}.json'.format(term), 'w').write( json.dumps(data, indent=2, ensure_ascii=False) )
+    except Exception as ex:
+      print(ex)
 
 arrs = {}
-for index, (term, freq) in enumerate(sorted(term_freq.items(), key=lambda x:x[1]*-1)):
-  key = index%32
+for index, term in enumerate(nouns):
+  key = index%64
   if arrs.get(key) is None:
     arrs[key] = []
   arrs[key].append(term)
 arrs = [(key, terms) for key, terms in arrs.items()]
-with concurrent.futures.ProcessPoolExecutor(max_workers=32) as exe:
+#_map(arrs[0])
+with concurrent.futures.ProcessPoolExecutor(max_workers=64) as exe:
   exe.map(_map, arrs)
