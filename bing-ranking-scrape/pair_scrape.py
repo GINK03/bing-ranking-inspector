cefbda43b6c9d72a758681ce6cd4004917d54df1
nardtree <gim.kobayashi@gmail.com>
2018-02-02 08:00:00 +0900

    詳細を編集

--- bing-ranking-scrape/pair_scrape.py
+++ bing-ranking-scrape/pair_scrape.py
@@ -11,19 +11,21 @@ def _map(arg):
   term, data = arg
   try:
     print(term, data)
-    top  = data[0]
-    tail = data[1]
-    headers = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
 
-    r = requests.get(top, headers=headers)
-    r.encoding = r.apparent_encoding
-    html = r.text
-    open('htmls/{}_top'.format(term), 'w').write( html )
-    
-    r = requests.get(tail, headers=headers)
-    r.encoding = r.apparent_encoding
-    html = r.text
-    open('htmls/{}_tail'.format(term), 'w').write( html )
+    if len(data) == 3:
+      args = [(1, 0), (2, 1), (3, 0)]
+    else:
+      args = [(1, 0), (2, 1)]
+
+    for rank, index in args:
+      url = data[index]
+      headers = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
+
+      r = requests.get(url, headers=headers)
+      r.encoding = r.apparent_encoding
+      html = r.text
+      open(f'htmls/{term}_{rank}', 'w').write( html )
+      
   except Exception as ex:
     print(ex)
 
