adb163a5a66567267f5f9b6a39332c0734f83a20
nardtree <gim.kobayashi@gmail.com>
2018-02-04 15:45:05 +0900

    修正

--- 30-index.py
+++ 30-index.py
@@ -32,11 +32,22 @@ open("term_index.json", "w").write( json.dumps(term_index, indent=2, ensure_asci
 
 fdata = open("./rank/train.data", "w")
 fgroup = open("./rank/train.group", "w")
-
+ftdata = open("./rank/test.data", "w")
+ftgroup = open("./rank/test.group", "w")
+import random
 for query, data in query_data.items():
-  size = len(data)
-  fgroup.write( '%d\n'%(size) )
-  for rank, wakati in data:
-    line = " ".join( ["%d:%d"%(term_index[term], freq) for term, freq in wakati.items()] )
-    print(rank, line)
-    fdata.write( str(rank) + " " + line + "\n" )
+  if random.random() < 0.05:
+    size = len(data)
+    ftgroup.write( '%d\n'%(size) )
+    for rank, wakati in data:
+      line = " ".join( ["%d:%d"%(term_index[term], freq) for term, freq in wakati.items()] )
+      print(rank, line)
+      ftdata.write( str(rank) + " " + line + "\n" )
+
+  else:
+    size = len(data)
+    fgroup.write( '%d\n'%(size) )
+    for rank, wakati in data:
+      line = " ".join( ["%d:%d"%(term_index[term], freq) for term, freq in wakati.items()] )
+      print(rank, line)
+      fdata.write( str(rank) + " " + line + "\n" )
